0d909511782df6809ac78e8205ef59c84b80194c
JBondeson19 <jpbond58@gmail.com>
2020-10-26 13:01:15 -0400

    "seed"

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,40 @@ Tag.destroy_all
 
 end
 
-50.times do
+150.times do
 
-    Dish.create(name: Faker::Food.dish)
+    Dish.create(name: Faker::Food.dish, restaurant_id: rand(121...140))
 
 end
+
+Tag.create(name: "Spicy")
+Tag.create(name: "Gluten")
+Tag.create(name: "Vegetarian")
+Tag.create(name: "Dairy")
+Tag.create(name: "Eggs")
+Tag.create(name: "Peanuts")
+Tag.create(name: "Raw")
+Tag.create(name: "Grass-Fed")
+Tag.create(name: "Farm-raised")
+Tag.create(name: "Tree-nuts")
+Tag.create(name: "Alliums")
+Tag.create(name: "Nightshade")
+
+DishTag.create(tag_id: 86,:dish_id:801)
+DishTag.create(tag_id: 90, dish_id: 801)
+DishTag.create(tag_id: , dish_id: )
+DishTag.create(tag_id:, dish_id: )
+DishTag.create(tag_id:, dish_id: )
+DishTag.create(tag_id:, dish_id: )
+DishTag.create(tag_id:, dish_id: )
+DishTag.create(tag_id:, dish_id: )
+DishTag.create(tag_id:, dish_id: )
+DishTag.create(tag_id:, dish_id: )
+DishTag.create(tag_id:, dish_id: )
+DishTag.create(tag_id:, dish_id: )
+DishTag.create(tag_id:, dish_id: )
+DishTag.create(tag_id:, dish_id: )
+DishTag.create(tag_id:, dish_id: )
+DishTag.create(tag_id:, dish_id: )
+DishTag.create(tag_id:, dish_id: )
+DishTag.create(tag_id:, dish_id: )
